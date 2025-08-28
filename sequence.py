46b9226997f729b97484e587b4e855ba5f744104
Levin Thijs <levinthijs@gmail.com>
2025-08-28 23:41:06 +0200

    Update sequence.py

--- sequence.py
+++ sequence.py
@@ -31,32 +31,72 @@ def split_in_codons(dna_sequence):
 
 # Codon dictionary
 codon_table = {
-    'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
-    'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
-    'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
-    'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
-    'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L', 'TTA':'L', 'TTG':'L',
-    'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
-    'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
-    'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
-    'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
-    'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A',
-    'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E',
-    'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G',
-    'TGC':'C', 'TGT':'C', 'TGG':'W',
-    'TAC':'Y', 'TAT':'Y',
+    # Alanine
+    'GCT':'A', 'GCC':'A', 'GCA':'A', 'GCG':'A',
+    # Arginine
+    'CGT':'R', 'CGC':'R', 'CGA':'R', 'CGG':'R', 'AGA':'R', 'AGG':'R',
+    # Asparagine
+    'AAT':'N', 'AAC':'N',
+    # Aspartic acid
+    'GAT':'D', 'GAC':'D',
+    # Cysteine
+    'TGT':'C', 'TGC':'C',
+    # Glutamine
+    'CAA':'Q', 'CAG':'Q',
+    # Glutamic acid
+    'GAA':'E', 'GAG':'E',
+    # Glycine
+    'GGT':'G', 'GGC':'G', 'GGA':'G', 'GGG':'G',
+    # Histidine
+    'CAT':'H', 'CAC':'H',
+    # Isoleucine
+    'ATT':'I', 'ATC':'I', 'ATA':'I',
+    # Leucine
+    'TTA':'L', 'TTG':'L', 'CTT':'L', 'CTC':'L', 'CTA':'L', 'CTG':'L',
+    # Lysine
+    'AAA':'K', 'AAG':'K',
+    # Methionine (start)
+    'ATG':'M',
+    # Phenylalanine
+    'TTT':'F', 'TTC':'F',
+    # Proline
+    'CCT':'P', 'CCC':'P', 'CCA':'P', 'CCG':'P',
+    # Serine
+    'TCT':'S', 'TCC':'S', 'TCA':'S', 'TCG':'S', 'AGT':'S', 'AGC':'S',
+    # Threonine
+    'ACT':'T', 'ACC':'T', 'ACA':'T', 'ACG':'T',
+    # Tryptophan
+    'TGG':'W',
+    # Tyrosine
+    'TAT':'Y', 'TAC':'Y',
+    # Valine
+    'GTT':'V', 'GTC':'V', 'GTA':'V', 'GTG':'V',
+    # Stop codons
     'TAA':'_', 'TAG':'_', 'TGA':'_'
 }
 
 
-def amino_seq(codons, codon_table):
-    sequence = []
-    for codon in codons:
-        aa = codon_table.get(codon, 'X')  # X for unknown codons (not present in codon_table)
-        if aa == '_':  # stopcodon
-            break
-        sequence.append(aa)
-    return ''.join(sequence) # sequence changes from list to string
+def translate_from_first_ATG(dna_sequence, codon_table):
+    """
+    Translates a DNA sequence into a protein sequence starting from the first ATG codon (start codon).
+    This ensures translation matches the actual protein (ignores UTRs before CDS).
+    """
+    dna_sequence = dna_sequence.upper().replace("\n","").replace(" ","")
+    
+    for i in range(0, len(dna_sequence)-2):
+        codon = dna_sequence[i:i+3]
+        if dna_sequence[i:i+3] == "ATG":  # find the first start codon
+            protein = []
+            for j in range(i, len(dna_sequence), 3):
+                codon = dna_sequence[j:j+3]
+                if len(codon) < 3:
+                    break
+                aa = codon_table.get(codon, 'X')  # unknown codons = 'X'
+                if aa == '_':  # stop codon
+                    return protein
+                protein.append(aa)
+            return protein
+    return []  # if no ATG found
 
 
 # Use
